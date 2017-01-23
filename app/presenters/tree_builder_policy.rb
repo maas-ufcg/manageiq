ae51a5dae94521c58f3bd3ed4febb28c71c93e40
Walter Alves <walter.arruda.alves@gmail.com>
2017-01-23 11:15:36 -0300

    Add i18n reference to 'Physical Server Policies'
    Added the i18n reference to Physical server policies in UI.

--- app/presenters/tree_builder_policy.rb
+++ app/presenters/tree_builder_policy.rb
@@ -19,12 +19,14 @@ class TreeBuilderPolicy < TreeBuilder
 
   def compliance_control_kids(mode)
     text_i18n = {:compliance => {:Host                => N_("Host Compliance Policies"),
+                                 :PhysicalServer      => N_("Physical Server Compliance Policies"),
                                  :Vm                  => N_("Vm Compliance Policies"),
                                  :ContainerReplicator => N_("Replicator Compliance Policies"),
                                  :ContainerGroup      => N_("Pod Compliance Policies"),
                                  :ContainerNode       => N_("Container Node Compliance Policies"),
                                  :ContainerImage      => N_("Container Image Compliance Policies")},
                  :control    => {:Host                => N_("Host Control Policies"),
+                                 :PhysicalServer      => N_("Physical Server Control Policies"),
                                  :Vm                  => N_("Vm Control Policies"),
                                  :ContainerReplicator => N_("Replicator Control Policies"),
                                  :ContainerGroup      => N_("Pod Control Policies"),
